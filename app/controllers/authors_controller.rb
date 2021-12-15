4ef657488e4a6a3aea95977c5386f418871a5258
Yuyi <yuyi365@gmail.com>
2021-12-15 14:59:01 -0500

    commit tree 16b7dc2c0d676d7e5b70b055c27e1d7914454d09

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,19 +2,18 @@ class AuthorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    authors = Author.all 
-    render json: authors
+    authors = Author.all
+    render json: authors, include: %w[profile posts posts.tags]
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
+    render json: author, include: %w[profile posts posts.tags]
   end
 
   private
 
   def render_not_found_response
-    render json: { error: "Author not found" }, status: :not_found
+    render json: { error: 'Author not found' }, status: :not_found
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :tags, only: [:index, :show]
-  resources :posts, only: [:index, :show]
-  resources :authors, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tags, only: %i[index show]
+  resources :posts, only: %i[index show]
+  resources :authors, only: %i[index show]
 end
